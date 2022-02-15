936182e9ff70748054c53140e10cb76c2d583175
WASD6570 <wasd12.ns@gmail.com>
2022-02-15 14:01:48 -0300

    ipn test

--- pages/api/ipn/mercadopago.ts
+++ pages/api/ipn/mercadopago.ts
@@ -6,6 +6,7 @@ export default methods({
   async post(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     const body = req.body;
     const query = req.query;
+    console.log({ body, query });
     res.status(200).json({ message: "testeanding", body, query });
   },
 });
